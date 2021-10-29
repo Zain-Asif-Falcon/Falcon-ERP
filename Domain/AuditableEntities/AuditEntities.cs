2ffbaf95a816cf7ba251ce448af8a4fda193691d
Zain-Asif-Falcon <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2021-10-29 18:24:52 +0500

    Add files via upload

---
+++ Domain/AuditableEntities/AuditEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.AuditableEntities
+{
+    public class AuditEntities
+    {
+        public DateTime created_at { get; set; }
+        public DateTime? updated_at { get; set; }
+        public DateTime? deleted_at { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeletable { get; set; }
+    }
+}
