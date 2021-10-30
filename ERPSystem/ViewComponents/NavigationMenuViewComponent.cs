ca49e32c0cd51c84e34e55c13f918c122afcad7c
Zain-Asif-Falcon <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2021-10-31 00:51:44 +0500

    Add files via upload

---
+++ ERPSystem/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Application.Business.Administration.IAppServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPSystem.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+	{
+		private readonly IDataAccessService _dataAccessService;
+
+		public NavigationMenuViewComponent(IDataAccessService dataAccessService)
+		{
+			_dataAccessService = dataAccessService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var items = await _dataAccessService.GetMenuItemsAsync(HttpContext.User);
+			
+			return View(items);
+		}
+	}
+}
