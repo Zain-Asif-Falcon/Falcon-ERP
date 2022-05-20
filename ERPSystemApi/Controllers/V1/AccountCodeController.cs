6ccc19e0ed2332f01544c8f36e924bc7168d0f74
Zain-Asif-Falcon <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2022-05-20 17:29:29 +0500

    Added comments

--- ERPSystemApi/Controllers/V1/AccountCodeController.cs
+++ ERPSystemApi/Controllers/V1/AccountCodeController.cs
@@ -22,24 +22,32 @@ namespace ERPSystemAPI.Controllers.V1
             _logger = logger;
             _IUnitOfWork = unitOfWork;
         }
+        
+        //Get All Account Codes
         [HttpGet(ApiRoutes.AccountCode.GetAll)]
         public async Task<IActionResult> GetAll()
         {
             var companyInfo = await _IUnitOfWork.AccCode.GetAllAccountCodes();
             return Ok(companyInfo);
         }
+        
+        //Get Active account codes only. These will be used in the application
         [HttpGet(ApiRoutes.AccountCode.GetActives)]
         public async Task<IActionResult> GetActives()
         {
             var companyInfo = await _IUnitOfWork.AccCode.GetAllAccountCodes();
             return Ok(companyInfo);
         }
+        
+        //Get Non-Active Account Codes. So Admin can reactivate the account code. 
         [HttpGet(ApiRoutes.AccountCode.GetNonActives)]
         public async Task<IActionResult> GetNonActives()
         {
             var companyInfo = await _IUnitOfWork.AccCode.GetAll(filter: x => x.IsActive == false);
             return Ok(companyInfo);
         }
+        
+        //Get Account Code by AccountCodeId
         [HttpGet(ApiRoutes.AccountCode.Get)]
         public async Task<IActionResult> Get(int accountcodeId)
         {
@@ -57,6 +65,7 @@ namespace ERPSystemAPI.Controllers.V1
             }
         }
 
+        //Delete an account code by Id.
         [HttpDelete(ApiRoutes.AccountCode.Delete)]
         public async Task<IActionResult> Delete(int accountcodeId)
         {
@@ -70,6 +79,8 @@ namespace ERPSystemAPI.Controllers.V1
                 return BadRequest(ex.ToString());
             }
         }
+        
+        //Update account codes here.
         [HttpPut(ApiRoutes.AccountCode.Update)]
         public async Task<IActionResult> Update([FromBody] AccountCode accCod)
         {
@@ -83,6 +94,8 @@ namespace ERPSystemAPI.Controllers.V1
                 return BadRequest(ex.ToString());
             }
         }
+        
+        //create new account codes.
         [HttpPost(ApiRoutes.AccountCode.Create)]
         public async Task<IActionResult> Create([FromBody] AccountCode accCod)
         {
@@ -96,12 +109,16 @@ namespace ERPSystemAPI.Controllers.V1
                 return BadRequest(ex.ToString());
             }
         }
+        
+        //Get Account codes to show in dropdowns. Only AccountCodeId and Account Code Name is returned
         [HttpGet(ApiRoutes.AccountCode.Dropdown)]
         public async Task<IActionResult> Dropdown()
         {
             var companyInfo = await _IUnitOfWork.AccCode.GetListAccountCodeForDropDown();
             return Ok(companyInfo);
         }
+        
+        //Check if an account code is already existing or not.
         [HttpGet(ApiRoutes.AccountCode.ChkExisting)]
         public async Task<IActionResult> GetExistingCode(string Code)
         {
@@ -109,6 +126,7 @@ namespace ERPSystemAPI.Controllers.V1
             return Ok(companyInfo);
         }
         //==================================================================
+        //Get All account code reports 
         [HttpGet(ApiRoutes.AccountCode.GetAllRepo)]
         public async Task<IActionResult> GetAllRepo()
         {
