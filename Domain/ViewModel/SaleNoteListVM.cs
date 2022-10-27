91a5479837901901c15cb5180d70f0355952859a
Zain-Asif-Falcon <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2022-10-27 17:08:34 +0500

    Add files via upload

---
+++ Domain/ViewModel/SaleNoteListVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModel.Transactions
+{
+    public class SaleNoteListVM
+    {
+        public int SalesGoodsId { get; set; }
+        public string DocumentNumber { get; set; }        
+        public string CustomerName { get; set; }        
+        public DateTime? DateSales { get; set; }
+        public DateTime? DueDate { get; set; }        
+        public decimal? InvoiceAmount { get; set; }        
+        public decimal? GrandTotal { get; set; }        
+        public decimal? PayingAmount { get; set; }
+        public decimal? TotalExpenses { get; set; }
+    }
+}
---
+++ Domain/ViewModel/SalesOrderViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Domain.ViewModel.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModel.Transactions
+{
+    public class SalesOrderViewModel
+    {
+       public SalesOrderViewModel()
+        {
+            order = new SalesGoods();
+            items = new List<SalesItems>();
+        }
+        public PartyCodes partyDet { get; set; }
+        public SalesGoods order { get; set; }
+        public List<SalesItems> items { get; set; }
+    }
+}
