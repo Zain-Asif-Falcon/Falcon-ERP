28663d6c1c15679111340a831c1fa66a2c3425c1
Zain-Asif-Falcon <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2022-10-27 17:02:55 +0500

    Add files via upload

---
+++ Application/Business/IAppServices/ISalesOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interface.Repositories;
+using Domain.Entities;
+using Domain.ViewModel.Reports;
+using Domain.ViewModel.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Business.Transactions.IAppServices
+{
+    public interface ISalesOrderRepository : IRepository<SalesGoods>
+    {
+        Task<List<SaleNoteListVM>> GetSaleOrders();
+        Task<bool> PlaceSaleOrder(SalesOrderViewModel purOrder);
+    }
+}
