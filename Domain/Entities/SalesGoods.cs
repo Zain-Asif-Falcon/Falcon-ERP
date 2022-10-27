8d5695bf2a5b83226325497acbed52e1976bf405
Zain-Asif-Falcon <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2022-10-27 17:11:15 +0500

    Add files via upload

---
+++ Domain/Entities/SalesGoods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class SalesGoods
+    {
+        public SalesGoods()
+        {
+            SalesItems = new HashSet<SalesItems>();
+        }
+        [Key]
+        public int SalesGoodsId { get; set; }
+        public string DocumentNumber { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Party Code Required")]
+        public int? AccountCodeId { get; set; }
+        [Required]
+        public DateTime? DateSales { get; set; }
+        public DateTime? DueDate { get; set; }
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Amount Should be Greater than 0")]
+        [Display(Prompt = "Products Sum Value")]
+        public decimal? InvoiceAmount { get; set; }
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Amount Should be Greater than 0")]
+        public decimal? GrandTotal { get; set; }
+        [Required(ErrorMessage = "Recieving Amount Required")]
+        [Range(0, Int32.MaxValue, ErrorMessage = "Amount Should be Greater than 0")]
+        public decimal? PayingAmount { get; set; }
+        public decimal? Labour { get; set; }
+        public decimal? Loading { get; set; }
+        public decimal? Cutting { get; set; }
+        public decimal? Carriage { get; set; }
+        public decimal? Carrogation { get; set; }
+        public decimal? Misc { get; set; }
+        public decimal? TotalExpenses { get; set; }
+        public string ReferenceNum { get; set; }
+        public string Notes { get; set; }
+        public int? DayOpeningsId { get; set; }
+        public bool? Acknowledged { get; set; }
+        public DateTime? DateTimeEntered { get; set; }
+        public virtual DayOpenings DayOpening { get; set; }
+        public virtual AccountCode AccountCode { get; set; }
+        public virtual ICollection<SalesItems> SalesItems { get; set; }
+    }
+}
---
+++ Domain/Entities/SalesItems.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+   public class SalesItems
+    {
+        [Key]
+        public int SalesItemsId { get; set; }
+        public int? SalesGoodsOrderId { get; set; }
+        [Required]
+        [Display(Name = "Item", Prompt = "Item Required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Item Required")]
+        public int? ItemsId { get; set; }
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Quantity should be greater than or equal to 1")]
+        [Display(Name = "Quantity", Prompt = "Qty Required")]
+        public int? Qty { get; set; }
+        public decimal? SalePrice { get; set; }
+        public string Notes { get; set; }
+        public DateTime? DateTimeEntered { get; set; }
+        public virtual Item Items { get; set; }
+        public virtual SalesGoods SalesGoodsOrder { get; set; }
+    }
+}
