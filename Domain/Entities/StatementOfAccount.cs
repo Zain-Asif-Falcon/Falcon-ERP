628a8892678592f6bff2ffdd7d61d317500af973
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-21 16:49:51 +0500

    Create StatementOfAccount.cs

---
+++ Domain/Entities/StatementOfAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class StatementOfAccount
+    {
+        [Key]
+        public int StatementOfAccountId { get; set; }
+        public int? AccountCodeId { get; set; }
+        public DateTime date { get; set; }
+        public string DocNo { get; set; }
+        public string Description { get; set; }  
+        public int? ReportType { get; set; }
+        public virtual AccountCode AccountCode { get; set; }
+    }
+}
