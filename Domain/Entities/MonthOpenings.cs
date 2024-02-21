e5aab36e2e31b615e7fb42829364d922f8de00b9
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-21 17:12:41 +0500

    Create MonthOpenings.cs

---
+++ Domain/Entities/MonthOpenings.cs
@@ -0,0 +1,21 @@
+﻿using Domain.AuditableEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class MonthOpenings : AuditEntities
+    {
+        [Key]
+        public int MonthOpeningsId { get; set; }        
+        public string Name { get; set; }
+        [Required]
+        public DateTime DateOpening { get; set; }       
+        [RegularExpression("^[a-zA-Z ]*$")]
+        public string Notes { get; set; }
+    }
+}
