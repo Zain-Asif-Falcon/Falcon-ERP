5d38cc18f4f5b91a213cbc4c6dfb29bbdc0f2a12
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-21 16:16:01 +0500

    Create DayOpenings.cs

---
+++ Domain/Entities/DayOpenings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class DayOpenings
+    {
+        [Key]
+        public int DayOpeningsId { get; set; }
+        public int? MonthOpeningsId { get; set; }
+        //[Required]
+        public DateTime DateTimeOpening { get; set; }
+     
+        public decimal? cashDifference { get; set; }
+        public decimal? ClosingBalance { get; set; }
+        public bool? closeStatus { get; set; }
+        //[RegularExpression("^[a-zA-Z ]*$")]
+        public string Notes { get; set; }
+        public virtual MonthOpenings MonthOpening { get; set; }
+    }
+}
